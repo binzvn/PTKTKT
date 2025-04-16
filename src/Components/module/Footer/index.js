08fc6d837ede8808283627b3ddae3f74f2addde6
Delicious <63702500+binzvn@users.noreply.github.com>
2025-04-17 00:18:21 +0700

    index.js

--- src/Components/module/Footer/index.js
+++ src/Components/module/Footer/index.js
@@ -17,8 +17,8 @@ const Footer = () => {
             <img src={logo} alt="" />
             <span className="ms-3 fs-5 fw-bolder">Ankasa</span>
             <p className="mt-3 text-secondary">
-              Find your Flight and explore the <br></br>
-              world with us. We will take care of the rest
+              Tìm kiếm chuyến bay của bạn <br></br>
+              Nhanh chóng và tiện lợi, tận hưởng trọn vẹn đường đi.
             </p>
           </div>
           <p className="text-secondary bd-highlight mt-6">
@@ -26,20 +26,20 @@ const Footer = () => {
           </p>
         </div>
         <div className="">
-          <p className="fw-bolder">Features</p>
-          <p className="text-secondary">Find Ticket</p>
-          <p className="text-secondary">My Booking</p>
-          <p className="text-secondary">Chat</p>
-          <p className="text-secondary">Notification</p>
+          <p className="fw-bolder">Tính năng</p>
+          <p className="text-secondary">Tìm kiếm chuyến bay</p>
+          <p className="text-secondary">Vé của tôi</p>
+          <p className="text-secondary">Hỗ trợ</p>
+          <p className="text-secondary">Thông báo</p>
         </div>
         <div className="d-flex flex-column">
-          <p className="fw-bolder">Download Angkasa App</p>
+          <p className="fw-bolder">Tải App Angkasa</p>
           <img src={google} alt="" />
           <img className="mt-2" src={app} alt="" />
         </div>
         <div className="d-flex flex-column bd-highlight">
           <div className="mb-auto bd-highlight">
-            <p className="fw-bolder">Follow Us</p>
+            <p className="fw-bolder">Theo dõi chúng tôi</p>
             <div className="d-flex">
               <img className="" src={facebook} alt="" />
               <img className="mx-3" src={twitter} alt="" />
@@ -62,26 +62,26 @@ const Footer = () => {
             <img src={logo} alt="" />
             <span className="ms-3 fs-5 fw-bolder">Ankasa</span>
             <p className="mt-4 text-secondary lh-base">
-              Find your Flight and explore the <br></br>
-              world with us. We will take care of the rest
+              Tìm kiếm chuyến bay của bạn<br></br>
+              Nhanh chóng và tiện lợi, tận hưởng trọn vẹn đường đi.
             </p>
           </div>
         </div>
-        <p className="fw-bolder mt-3">Features</p>
+        <p className="fw-bolder mt-3">Tính năng</p>
         <div className="d-flex justify-content-between">
-          <p className="text-secondary">Find Ticket</p>
-          <p className="text-secondary">My Booking</p>
-          <p className="text-secondary">Chat</p>
-          <p className="text-secondary">Notification</p>
+          <p className="text-secondary">Tìm kiếm chuyến bay</p>
+          <p className="text-secondary">Vé của tôi</p>
+          <p className="text-secondary">Hỗ trợ</p>
+          <p className="text-secondary">Thông báo</p>
         </div>
-        <p className="fw-bolder mt-5">Download Angkasa App</p>
+        <p className="fw-bolder mt-5">Tải app Ankasa</p>
         <div className="d-flex">
           <img src={google} alt="" />
           <img className="ms-3" src={app} alt="" />
         </div>
         <div className="d-flex flex-column bd-highlight">
           <div className="mb-auto bd-highlight mt-5">
-            <p className="fw-bolder">Follow Us</p>
+            <p className="fw-bolder">Theo dõi chúng tôi</p>
             <div className="d-flex">
               <img className="" src={facebook} alt="" />
               <img className="mx-3" src={twitter} alt="" />
